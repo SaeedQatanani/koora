1f45ea357800b4e119ea05adc8ce3fa5324c4029
SaeedQatanani <saeedqatananil@gmail.com>
2022-11-20 11:09:01 +0200

    initial commit

---
+++ koora/src/main/java/com/axsos/koora/KooraApplication.java
@@ -0,0 +1,13 @@
+package com.axsos.koora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KooraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KooraApplication.class, args);
+	}
+
+}
---
+++ koora/src/test/java/com/axsos/koora/KooraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsos.koora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KooraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
